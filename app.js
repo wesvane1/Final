9c8b0fa594ab69d1befc653c4e537e11f7ed6c6c
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2024-03-11 16:16:08 -0600

    reverted changes

--- app.js
+++ app.js
@@ -1,32 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-const { body } = require('express-validator');
-
-const port = process.env.PORT
 const app = express();
+
+const port = 3000;
 
 app.use('/', require('./routes'));
 
-app
-  .use('/api-docs', swaggerUi.serve, seaggerUi.setup(swaggerDocument))
-  .use(bodyParser.json())
-  .use((req, res, next) =>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-
-process.on('uncaughtException', (err, origin) => {
-  console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
-});
-  
-mongodb.initDb((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
-  }
+app.listen(process.env.PORT || port, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || port));
 });
