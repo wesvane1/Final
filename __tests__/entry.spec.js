f44dc1666110150ad39c4fb36ba7b6bdf69c65f2
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2024-04-03 14:08:17 -0600

    changes to get all routes working

--- __tests__/entry.spec.js
+++ __tests__/entry.spec.js
@@ -5,11 +5,11 @@ const request = supertest(app)
 
 
 describe('Entries', () => {
-    // test('responds to /entries', async () => {
-    //     const res = await request.get('/entries/getAll');
-    //     expect(res.header['content-type']).toBe('application/json; charset=utf-8');
-    //     expect(res.statusCode).toBe(200)
-    // })
+    test('responds to /entries', async () => {
+        const res = await request.get('/entries/getAll');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(200)
+    })
     test('responds to /entries', async () => {
         const res = await request.get('/entries/getSingle');
         expect(res.header['content-type']).toBe('application/json; charset=utf-8');
